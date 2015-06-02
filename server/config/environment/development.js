e6336f957c19aeda391e9a7a6a0447d38026e518
Vlada <pasechnik.vlada@gmail.com>
2015-06-03 02:38:09 +0300

    initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://root:root@ds041992.mongolab.com:41992/spc-data'
+  },
+
+  seedDB: true
+};
