1ed8a57ef6c35e06688f6d7e7488b3a954597c04
Vlada <pasechnik.vlada@gmail.com>
2015-06-11 23:50:27 +0300

    favicon

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ var app = express();
 var server = require('http').createServer(app);
 require('./config/express')(app);
 require('./routes')(app);
-
+app.use(favicon(__dirname + '/public/favicon.ico'));
 // Start server
 server.listen(config.port, config.ip, function () {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
